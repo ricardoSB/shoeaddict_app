0f1a44d160959453d3f7c768bdd1d6ffc25efa7c
Juan Ruiz <invitado@perdidita.(none)>
2014-04-04 19:15:40 +0100

    Finish uploading files to S3 with CarrierWave

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoeaddictApp::Application.config.secret_key_base = '96666e60d00d3808ea3b62a8bbc058a5a54e3d7d2b58a8ff0853ece94a700d51e1b65b7d598dbda66273d5592d1f012e9f1a45f617db6deaca05de0c6f5f152b'
